b8221bc1e5c3183bf29ef84ac4dbc3e95e1e7e09
stailgot <sanchesz@list.ru>
2017-02-16 01:29:54 +0300

    theme selection

--- dzenf/Program.fs
+++ dzenf/Program.fs
@@ -3,6 +3,8 @@ open System.Windows.Controls
 open System.Windows
 open UI.Window
 
+open MahApps.Metro
+
 type DataItem = { Name:string; Count:int }
 
 [<STAThread>]
@@ -11,12 +13,27 @@ let main argv =
     let app = UI.Application()
     let window = Window( Visibility = Visibility.Visible )
 
+    
+
     app.MainWindow <- window
     //let window = Window( Visibility = Visibility.Visible )
     let data = {1..1000000} |> Seq.map (fun e -> {Name = Convert.ToString e; Count = e}) |> Collections.ObjectModel.ObservableCollection
 
     window.ctrlDataGrid.ItemsSource <- data
+    //window.ctrlListBox.ItemsSource <- ThemeManager.Accents
+    window.ctrlListBox.ItemsSource <- ThemeManager.Accents |> Seq.map ( fun v -> v.Name )
+    //window.ctrlListBox2.ItemsSource <- ThemeManager.AppThemes
+    window.ctrlListBox2.ItemsSource <- ThemeManager.AppThemes |> Seq.map ( fun v -> v.Name )
+    window.ctrlListBox.SelectedIndex <- 0
+    window.ctrlListBox2.SelectedIndex <- 0
 
+    let set_theme () = ThemeManager.ChangeAppStyle(Application.Current,
+                                                   ThemeManager.GetAccent( Convert.ToString window.ctrlListBox.SelectedItem ),
+                                                   ThemeManager.GetAppTheme (Convert.ToString window.ctrlListBox2.SelectedItem) )
+
+    ignore >> set_theme |> window.ctrlListBox.SelectionChanged.Add
+    ignore >> set_theme |> window.ctrlListBox2.SelectionChanged.Add
+    set_theme()
     
     let async_add_data () =
       async{ Seq.initInfinite (fun e -> 
