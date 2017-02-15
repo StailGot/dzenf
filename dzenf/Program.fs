8a272c047600f39b58d296522f1791b7e28189c6
stailgot <sanchesz@list.ru>
2017-02-15 22:47:41 +0300

    simple grid

--- dzenf/Program.fs
+++ dzenf/Program.fs
@@ -1,10 +1,23 @@
-﻿open UI.Window
-open System
+﻿open System
 open System.Windows.Controls
 open System.Windows
+open UI.Window
+
+type DataItem = { Name:string; Count:int }
 
 [<STAThread>]
 [<EntryPoint>]
-let main argv = 
+let main argv =
     let window = Window( Visibility = Visibility.Visible )
-    System.Windows.Application( MainWindow = window ).Run()
+    let data = [1..10000000] |> Seq.map (fun e -> {Name = Convert.ToString e; Count = e}) |> Collections.ObjectModel.ObservableCollection
+    window.dataGrid.ItemsSource <- data
+    
+    async{ Seq.initInfinite (fun e -> 
+           printfn "asd"
+           System.Threading.Thread.Sleep 300;
+           window.dataGrid.Dispatcher.Invoke( fun () -> data.Add {Name = Convert.ToString e; Count = data.Count }; 
+                                                        window.dataGrid.SelectedIndex <- data.Count - 1
+                                                        window.dataGrid.ScrollIntoView ( window.dataGrid.SelectedItem )
+                                                        )) |> Seq.iter ignore
+          } |> Async.Start
+    Application( MainWindow = window ).Run()
