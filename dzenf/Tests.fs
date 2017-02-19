50069f5aae6f5b07fe2460ed7e0dacae86b6a97f
stailgot <sanchesz@list.ru>
2017-02-20 01:13:18 +0300

    tests

--- dzenf/Tests.fs
+++ dzenf/Tests.fs
@@ -4,6 +4,11 @@ module ``Base Tests Cases`` =
   open NUnit.Framework
   open FsUnit
 
+  let [<Literal>] s1 = "q"
+  let [<Literal>] s2 = "s"
+  let [<Literal>] s3 = "v"
+
   [<Test>]
-  let ``Readable test cae name`` () = 
+  let ``Readable test case name``
+    ([<Values(s1, s2, s3)>] i) = 
     true |> should be (equal true)
