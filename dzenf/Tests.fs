66d0273f4f0035714f8fc2781999080a89100dd5
stailgot <sanchesz@list.ru>
2017-02-20 23:43:06 +0300

    tests

--- dzenf/Tests.fs
+++ dzenf/Tests.fs
@@ -17,12 +17,16 @@ module ``Base Tests Cases`` =
     | Success( result, _, _) -> Some result
     | _ -> None
   let [<Literal>] s1 = @"some 42"
-  let [<Literal>] s2 = @"dsad 
-  dsa^%$% &#^ &SA 231sd23a4s0 dssa@!#$$4fa duas jjn v^$^$^%&V gjhg hascjbsma c some       
+  let [<Literal>] s2 = @"dsad++++++++++[>+++++++>++++++++++>+++<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.some       
   
   23  "
+  let [<Literal>] s3 = @"sooome 99"
 
   [<TestCase( s1, 42L )>]
   [<TestCase( s2, 23L )>]
   let ``Parse int in text``
     (text, expected) = get_revision text |> should equal (Some expected)
+
+  [<TestCase( s3 )>]
+  let ``Parse int in text: empty result``
+    (text) = get_revision text |> should equal None
